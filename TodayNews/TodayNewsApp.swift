5b9e8acf6eb583b8b7144fde43c82a1b631dc197
Mythrai Boga <mythrai.boga@walmart.com>
2025-03-13 12:46:45 +0530

    Initial Commit

---
+++ TodayNews/TodayNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodayNewsApp.swift
+//  TodayNews
+//
+//  Created by Mythrai Boga on 13/03/25.
+//
+
+import SwiftUI
+
+@main
+struct TodayNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
